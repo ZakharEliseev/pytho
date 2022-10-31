d9f4eb6b93c62bf79971a1c810b29083f9c1b98a
ezv <eliseev.zv@gmail.com>
2022-10-31 15:15:58 +0500

    demo zip work in prigress

--- les4/main.py
+++ les4/main.py
@@ -1,2 +1,14 @@
+def demo_zip():
+    values1 = list(range(2, 10))
+    values2 = list(range(10, 29, 3))
+    print(values1)
+    print(values2)
+
+
 def main():
-    print('Hello main!')
+    # print('Hello main!')
+    demo_zip()
+
+
+if __name__ == '__main__':
+    main()
