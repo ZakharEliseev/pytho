c7f8ae699edb1ee5ee5189310abad4032e426621
ezv <eliseev.zv@gmail.com>
2022-10-31 15:11:15 +0500

    demo map func (comment put demo zip)

--- les4/main.py
+++ les4/main.py
@@ -1,3 +1,13 @@
+def demo_map():
+    list_of_powers = list(
+        map(
+            lambda x: x ** 2,
+            [1, 2, 3, 4, 5]
+        )
+    )
+    print(list_of_powers)
+
+
 def demo_zip():
     values1 = list(range(2, 10))
     values2 = list(range(10, 29, 3))
@@ -10,7 +20,7 @@ def demo_zip():
 
 def main():
     demo_zip()
-
+    demo_map()
 
 if __name__ == '__main__':
     main()
