160479b362a2c695219cf20e88948804063585cf
ezv <eliseev.zv@gmail.com>
2022-11-22 16:31:49 +0500

    end

--- les9/db_helper.py
+++ les9/db_helper.py
@@ -18,4 +18,20 @@ def get_engine(url=None):
 def get_connection(engine=None):
     if engine is None:
         engine = get_engine()
-    return Connection(Engine)
+    return Connection(engine)
+
+
+class User:
+    def __init__(self, username: str):
+        self.username = username
+        self.age = None
+
+    def __str__(self):
+        return self.username
+
+    def set_age(self, age: int):
+        self.age = age
+
+    def delete(self):
+        print("delete", self)
+        return True
--- les9/testing/test_db_helper.py
+++ les9/testing/test_db_helper.py
@@ -1,21 +1,24 @@
-import pytest
+from string import ascii_lowercase
+from random import choices, randint, choice
 
+import pytest
 
 from db_helper import (
     Engine,
     Connection,
+    get_engine,
     get_connection,
-    get_engine
+    User,
 )
 
 
 @pytest.fixture
 def conn_url():
-    return 'conn_url'
+    return "conn_url"
 
 
 @pytest.fixture
-def engine_default() -> Engine:
+def engine_default(conn_url) -> Engine:
     return get_engine(url=conn_url)
 
 
@@ -24,8 +27,26 @@ def connection_default(engine_default) -> Connection:
     return get_connection(engine_default)
 
 
-def text_fixtures_connection(engine_default, connection_default, conn_url):
+def test_fixtures_connection(connection_default, engine_default, conn_url):
     assert isinstance(connection_default, Connection)
     assert isinstance(engine_default, Engine)
     assert connection_default.engine is engine_default
     assert engine_default.url == conn_url
+
+
+@pytest.fixture
+def user():
+    username = "".join(choices(ascii_lowercase, k=8))
+    user = User(username)
+    print("created user", user)
+    yield user
+
+    user.delete()
+
+
+class TestUser:
+    def test_set_age(self, user):
+        age = randint(1, 100)
+        assert user.age != age
+        user.set_age(age)
+        assert user.age == age
