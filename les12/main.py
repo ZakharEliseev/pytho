3811079c0399d884ff001139e6d06d6b884db88e
ZakharEliseev <eliseev.zv@gmail.com>
2022-12-08 11:21:23 +0500

    add

--- les12/main.py
+++ les12/main.py
@@ -1,1 +1,21 @@
-fedsfffffffadsdfs
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get('/hello')
+def hello():
+    return{
+        'message': 'Hello!!!!'
+    }
+
+
+@app.get('/{url_path:path}')
+def all_others(url_path: str):
+    return{'request to': url_path}
+
